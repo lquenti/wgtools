f8a94052de0f484c4210d98c213a03d31905ad7d
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2021-10-12 16:15:34 +0200

    :globe_with_meridians: English message, I should translate everything stupid idea to begin with

--- generate_trash_message/generate_trash_message.py
+++ generate_trash_message/generate_trash_message.py
@@ -8,7 +8,7 @@ WG_FILENAME: str = "wg.json"
 WG_FILEPATH: str = os.path.join(BASE_PATH, WG_FILENAME)
 
 def generate_message(who):
-    return f"Diese Woche hat {who} Putzdienst!"
+    return f"{who} has to bring out the trash this week!"
 
 def read_cfg():
     with open(WG_FILEPATH) as fp:
