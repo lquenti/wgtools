678b146b2dd2f3d9df8cbde379e4f58f51be18b5
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2021-10-12 16:33:50 +0200

    Use venv

--- send_trash_message.sh
+++ send_trash_message.sh
@@ -2,4 +2,6 @@
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+source "$SCRIPT_DIR/venv/bin/activate"
+
 python3 "$SCRIPT_DIR/generate_trash_message/generate_trash_message.py" | python3 "$SCRIPT_DIR/send_to_wg/send_to_wg.py"
