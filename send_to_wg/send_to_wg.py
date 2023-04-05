060acaf3efc3f9941ca55f61756912c9e92ac9a9
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-04-05 10:41:27 +0200

    send_to_wg new telegram client async

--- send_to_wg/send_to_wg.py
+++ send_to_wg/send_to_wg.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import sys
 import telegram
@@ -8,10 +9,11 @@ BASE_PATH: str = os.path.dirname(os.path.realpath(__file__))
 ENV_FILENAME: str = "env"
 ENV_FILEPATH: str = os.path.join(BASE_PATH, ENV_FILENAME)
 
-def main():
+async def main():
     data = sys.stdin.read().strip()
     cfg = dotenv_values(ENV_FILEPATH)
-    bot = telegram.Bot(token=cfg['TOKEN']).sendMessage(chat_id=cfg['CHAT_ID'], text=data)
+    bot = await telegram.Bot(token=cfg['TOKEN']).sendMessage(chat_id=cfg['CHAT_ID'], text=data)
 
 if __name__ == "__main__":
-    main()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
